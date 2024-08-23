98506389c392086f0f6fa4c24ef702f28f4d1962
Antoine Cheneau <antoine.cheneau@student-cs.fr>
2024-08-23 15:34:20 +0200

    feat(nix-darwin): add gui apps

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -10,8 +10,24 @@
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
+    let
+      configuration = { pkgs, ... }: {
+        homebrew.enable = true;
+        homebrew.casks = [
+          "arc"
+          "notion"
+          "notion-calendar"
+          "microsoft-outlook"
+          "microsoft-excel"
+          "microsoft-powerpoint"
+          "microsoft-teams"
+          "microsoft-word"
+          "telegram"
+          "whatsapp"
+          "slack"
+          "visual-studio-code"
+        ];
+        homebrew.onActivation.cleanup = "zap";
 
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
