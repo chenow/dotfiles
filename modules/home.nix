3f59b4f42ca140c023b82f48a942a723be989603
Antoine Cheneau <antoine.cheneau@student-cs.fr>
2024-08-27 10:45:54 +0200

    refacto(zsh): move configuration to nix

--- modules/home.nix
+++ modules/home.nix
@@ -9,15 +9,46 @@
   # release notes.
   home.stateVersion = "24.05";
 
-  home.packages = [ pkgs.git pkgs.fzf pkgs.vim pkgs.direnv ];
+  home.packages = with pkgs; [ git fzf vim direnv zsh ];
 
-  home.file = {
-    ".config/direnv/direnv.toml".source = ./confs/direnv.toml;
-    ".zshrc".source = ./confs/.zshrc;
+  home.file = { ".config/direnv/direnv.toml".source = ./confs/direnv.toml; };
+
+  home.sessionVariables = {
+    EDITOR = "code";
+    DOTFILES = "/Users/chenow/Documents/git/dotfiles";
   };
-
-  home.sessionVariables = { EDITOR = "code"; };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    syntaxHighlighting.enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" "docker" "macos" "direnv" ];
+      theme = "robbyrussell";
+
+    };
+    initExtra = ''
+      echo "Welcome to Oh My Zsh managed by Home Manager!"
+    '';
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  home.shellAliases = {
+    shell = "docker compose run --rm shell";
+    ga = "git add .";
+    gck = "git checkout";
+    grc = "git rebase --continue";
+    gski = "git stash --keep-index";
+    user-up = "home-manager switch --flake $DOTFILES";
+    system-up = "darwin-rebuild switch --flake $DOTFILES/nix-darwin";
+  };
 }
