18b2e0bc9a7a402d2484cd87f65a4b2e25916ddd
chenow <antoine.cheneau@outlook.com>
2024-09-13 21:12:13 +0200

    feat(git): add git autofixup alias

--- user-conf/git.nix
+++ user-conf/git.nix
@@ -1,7 +1,21 @@
+{ pkgs, ... }:
+
 {
   programs.git = {
     enable = true;
     userName = "chenow";
     userEmail = "antoine.cheneau@outlook.com";
+
+    aliases = {
+      autofixup = "!f() { \
+        commit=$(git log --oneline | ${pkgs.fzf}/bin/fzf --preview 'git show --color=always {1}' | cut -d' ' -f1); \
+        if [ -n \"$commit\" ]; then \
+            git commit --fixup=$commit && \
+            GIT_SEQUENCE_EDITOR=: git rebase -i --autostash --autosquash $commit~1; \
+        else \
+            echo \"No commit selected\"; \
+        fi; \
+    }; f";
+    };
   };
 }
