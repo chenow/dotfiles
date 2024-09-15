180f4180296c649c197c2f812643882059f83843
chenow <antoine.cheneau@outlook.com>
2024-09-15 12:31:37 +0200

    feat(mac-finder): add configuration

--- system-conf/finder.nix
+++ system-conf/finder.nix
@@ -3,4 +3,17 @@
 {
   system.defaults.NSGlobalDomain.AppleShowAllExtensions = true;
   system.defaults.NSGlobalDomain.AppleShowAllFiles = true;
+
+  system.defaults.finder = {
+    AppleShowAllExtensions = true;
+    AppleShowAllFiles = true;
+    CreateDesktop = false;
+    FXEnableExtensionChangeWarning = false;
+    FXPreferredViewStyle = "Nlsv";
+    QuitMenuItem = true;
+    ShowPathbar = true;
+    ShowStatusBar = true;
+    _FXShowPosixPathInTitle = false;
+    FXDefaultSearchScope = "SCcf";
+  };
 }
