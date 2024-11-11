b1611abdb2ecac962d1c622c45504f11cf68b55b
chenow <antoine.cheneau@outlook.com>
2024-11-11 09:52:14 +0100

    review packages to install

--- modules/darwin/casks.nix
+++ modules/darwin/casks.nix
@@ -6,21 +6,10 @@ _:
   "visual-studio-code"
 
   # Communication Tools
-  "discord"
-  "notion"
   "slack"
   "telegram"
   "zoom"
 
-  # Utility Tools
-  "syncthing"
-
-  # Entertainment Tools
-  "vlc"
-
-  # Productivity Tools
-  "raycast"
-
   # Browsers
-  "google-chrome"
+  "arc-browser"
 ]
